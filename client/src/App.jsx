52c02eb5b3451cd0aff26b8ad96c96916476ce4e
jeel105512 <jeelhpatel10@gmail.com>
2024-05-13 12:17:47 -0400

    Added header navigation component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home/Home";
 import Signin from "./pages/Signin/Signin";
 import SignUp from "./pages/SignUp/SignUp";
 import Profile from "./pages/Profile/Profile";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
