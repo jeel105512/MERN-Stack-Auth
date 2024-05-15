143adc82b4363e3cce5ae2db1168c741bd1a3d29
jeel105512 <jeelhpatel10@gmail.com>
2024-05-15 11:19:52 -0400

    Added the error handler middleware and a custom error handler util

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   try {
@@ -9,6 +10,6 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ success: true, status: 201, newUser });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
